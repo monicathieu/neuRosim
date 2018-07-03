52c90b1cfb51ffb2f21854429d0263de90bd6034
monicathieu <mthieu@stanford.edu>
2018-07-03 12:13:55 -0700

    wraps onsets as list at the beginning; assigns cond.names BEFORE final join

--- R/stimfunction.R
+++ R/stimfunction.R
@@ -4,7 +4,7 @@
 #' and microtime resolution based on the user-defined onsets and durations.
 #' 
 #' @export
-#' @importFrom dplyr if_else mutate n tibble %>%
+#' @importFrom dplyr if_else funs mutate n rename_at tibble %>%
 #' @importFrom purrr map map2 reduce rerun
 #' 
 #' @param totaltime Total time of the design in seconds.
@@ -24,7 +24,7 @@
 #' total <- 100
 #' os <- c(1, 21, 41, 61, 81)
 #' d <- 10
-#' out <- stimfunction(total, os, d, 0.1)
+#' out <- stimfunction(total, os, d, accuracy = 0.1)
 #' de\dontshow{rm(total,os,d,out)}
 #' 
 #' @keywords low-level
@@ -33,15 +33,31 @@ stimfunction <- function(totaltime, onsets, durations, cond.names = NULL, accura
   # TODO: allow stimfunction to output an object that is the input to specifydesign
   # with multiple onset conditions
   
-  if (length(unlist(durations)) == 1) { # in this case, all onsets/all conditions same duration
-    durations <- map(onsets, ~rep(durations, length(.)))
-  } else if (!is.list(durations) & length(durations) == length(onsets)) {
-    durations = map2(durations, onsets, ~rep(.x, length(.y)))
+  # these two conditionals should stop when c() or a list with all entries NULL are input for onsets
+  if (length(onsets) < 1) stop("Must specify more than zero onsets.")
+  if (sum(lengths(onsets)) < 1) stop("Must specify more than zero onsets.")
+  
+  if (!is.list(onsets)) onsets <- list(onsets) # if input as num vector, get into 1 list field per condition format
+  
+  if (length(unlist(durations)) == 1) {
+    # in this case, all onsets/all conditions same duration
+    durations <- map(onsets, ~rep(unlist(durations), length(.)))
+  } else if (!is.list(durations)) {
+    # each duration corresponds to an onset condition
+    if (length(durations) == length(onsets)) {
+      durations <- map2(durations, onsets, ~rep(.x, length(.y)))
+    } else if (length(onsets) == 1 & length(durations) == lengths(onsets)) {
+      # need this special case now that onsets are always coerced to list
+      # if one onset condition, and a duration is specified individually for each onset
+      durations <- list(durations)
+    }
   } else if (is.list(durations) & length(durations) == length(onsets)) {
+    # each list-field of duration corresponds to an onset condition
     if (all(lengths(durations) == 1)) {
-      durations <- map2(durations, onsets, ~rep(.x, length(.)))
+      # if every onset within condition has the same onset:
+      durations <- map2(durations, onsets, ~rep(.x, length(.y)))
     } else if (all(lengths(durations) == lengths(onsets))) {
-      
+      # do nothing
     } else {
       stop("Mismatch between onsets and durations.")
     }
@@ -49,14 +65,12 @@ stimfunction <- function(totaltime, onsets, durations, cond.names = NULL, accura
     stop("Mismatch between onsets and durations.")
   }
   
-  if (max(unlist(onsets)) > totaltime) {
-    stop("Mismatch between onsets and totaltime")
-  }
+  if (max(unlist(onsets)) > totaltime) stop("Mismatch between onsets and totaltime.")
   
-  if (is.null(cond.names)) {
-    n.conds <- if_else(is.list(onsets), length(onsets), 1L)
-    cond.names <- paste0("C", 1:n.conds)
-  }
+  if (is.null(cond.names)) cond.names <- paste0("C", 1:length(onsets))
+  
+  if (length(cond.names) != length(onsets)) stop("Mismatch between # onset conditions & # condition names.")
+
   
   # There's an under/overflow issue such that we can't rely on using microtime itself as the index column
   out <- tibble(cond.names = cond.names,
@@ -68,8 +82,10 @@ stimfunction <- function(totaltime, onsets, durations, cond.names = NULL, accura
            full = map2(full, params, ~.x %>%
                          mutate(s = if_else(microtime %in% unlist(.y$micro), 1L, 0L),
                                 # need to get it back out of integers into real seconds
-                                microtime = microtime * accuracy)))
+                                microtime = microtime * accuracy)),
+           # must de-duplicate names BEFORE the full_join call for it to work with >2 conditions
+           full = map2(full, cond.names, ~rename_at(.x, "s", funs(paste(., .y, sep = ".")))))
   
-  return(reduce(out$full, full_join, by = "microtime", suffix = paste0(".", cond.names)))
+  return(reduce(out$full, full_join, by = "microtime"))
 }
 
